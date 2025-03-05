dbeba9fb451886973a75b685e85b276a0436e9e1
Ericdte <eric.djou41@gmail.com>
2025-03-06 00:55:23 +0100

    Update GitHub Actions workflow and add basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders delivery app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/delivery/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders without crashing', () => {
+  render(<App />);
+  expect(document.body).toBeInTheDocument();
 });
